6b8b9eb280b50a062d469f8d797b05fcb3e86027
Ikko-T <skyblueboy32@gmail.com>
2022-03-04 17:05:02 +0900

    Set up a current time

--- js/today.js
+++ js/today.js
@@ -1,9 +1,18 @@
-const now = new Date();
-const year = now.getFullYear();
-const month = now.getMonth();
-const date = now.getDate();
-const hour = now.getHours();
-const minute = now.getMinutes();
-const day = now.getDay();
+function setTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const date = now.getDate().toString().padStart(2, '0');
+  const hour = now.getHours().toString().padStart(2, '0');
+  const minute = now.getMinutes().toString().padStart(2, '0');
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const today = days[now.getDay()];
+  const currentTime =
+    'It is ' + hour + ':' + minute + ', ' + today + ', ' + month + '-' + date + '-' + year + ' in Japan';
 
-console.log(year, month, date, hour, minute, day);
+  document.getElementById('current-time').textContent = currentTime;
+}
+
+setInterval(function () {
+  setTime();
+}, 1000);
