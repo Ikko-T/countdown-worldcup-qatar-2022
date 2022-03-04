2f3f18cee647660fb21864509e9701c49c867094
Ikko-T <skyblueboy32@gmail.com>
2022-03-04 10:55:56 +0900

    Create a new file for rihgt-now-time

---
+++ js/today.js
@@ -0,0 +1,9 @@
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth();
+const date = now.getDate();
+const hour = now.getHours();
+const minute = now.getMinutes();
+const day = now.getDay();
+
+console.log(year, month, date, hour, minute, day);
